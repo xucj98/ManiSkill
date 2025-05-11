722171df274b70247e6c06895efa4584eb5aa7e0
Xu Cuijie <975114697@qq.com>
2025-05-11 18:20:43 +0800

    完成 ODPCDataset

---
+++ examples/baselines/diffusion_policy/diffusion_policy/odpc_dataset.py
@@ -0,0 +1,127 @@
+import h5py
+import numpy as np
+from tqdm import tqdm
+from torch.utils.data.dataset import Dataset
+from gymnasium import spaces
+
+
+def reorder_keys(d, ref_dict):
+    out = dict()
+    for k, v in ref_dict.items():
+        if k not in d:
+            continue
+        if isinstance(v, dict) or isinstance(v, spaces.Dict):
+            out[k] = reorder_keys(d[k], ref_dict[k])
+        else:
+            out[k] = d[k]
+    return out
+
+
+class ODPCDataset(Dataset):
+    def __init__(self, args, data_path, obs_process_fn, obs_space, include_rgb, include_depth, num_traj):
+        self.data_path = data_path
+        self.obs_process_fn = obs_process_fn
+        self.obs_space = obs_space
+        self.include_rgb = include_rgb
+        self.include_depth = include_depth
+        self.control_mode = args.control_mode
+
+        self.obs_horizon, self.pred_horizon = obs_horizon, pred_horizon = (
+            args.obs_horizon,
+            args.pred_horizon,
+        )
+
+        with h5py.File(self.data_path, "r") as file:
+            keys = list(file.keys())
+            if num_traj is not None:
+                assert num_traj <= len(keys), f"num_traj: {num_traj} > len(keys): {len(keys)}"
+                keys = sorted(keys, key=lambda x: int(x.split("_")[-1]))
+                keys = keys[:num_traj]
+
+            self.traj_keys = keys
+            self.traj_lens = []
+            self.slices = []
+            total_transitions = 0
+
+            pbar = tqdm(total=len(keys), desc="Prepare dataset.")
+
+            for traj_idx, traj_key in enumerate(self.traj_keys):
+                traj_len = file[f'{traj_key}/actions'].shape[0]
+                self.traj_lens.append(traj_len)
+                total_transitions += traj_len
+
+                # poses_cam0_peg = file[f'{traj_key}/obs/extra/cam0_peg_pose']
+                poses_world_peg = file[f'{traj_key}/obs/extra/peg_pose']
+                # cam0_extrinsic = file[f'{traj_key}/obs/sensor_param/base_camera/extrinsic_cv']
+
+                peg_z = poses_world_peg[:-1, 2]
+                peg_z = peg_z - np.min(peg_z)
+                traj_start = np.where(peg_z > 1e-3)[0][0]
+
+                # |o|o|                             observations: 2
+                # | |a|a|a|a|a|a|a|a|               actions executed: 8
+                # |p|p|p|p|p|p|p|p|p|p|p|p|p|p|p|p| actions predicted: 16
+                pad_after = pred_horizon - obs_horizon
+                # Pad after the trajectory, so all the observations are utilized in training
+                # Note that in the original code, pad_after = act_horizon - 1, but I think this is not the best choice
+                self.slices += [
+                    (traj_idx, start, start + pred_horizon)
+                    for start in range(traj_start, traj_len - pred_horizon + pad_after)
+                ]  # slice indices follow convention [start, end)
+
+                pbar.update(1)
+
+        print(
+            f"Total transitions: {total_transitions}, Total obs sequences: {len(self.slices)}"
+        )
+
+        self._h5_file = None
+
+    def _ensure_h5_open(self):
+        if self._h5_file is None:
+            self._h5_file = h5py.File(self.data_path, 'r')
+
+    def __getitem__(self, index):
+        self._ensure_h5_open()
+
+        traj_idx, start, end = self.slices[index]
+        traj_key = self.traj_keys[traj_idx]
+        L = self.traj_lens[traj_idx]
+
+        def get_slice_data(file):
+            if isinstance(file, (h5py.File, h5py.Group)):
+                return {key: get_slice_data(file[key]) for key in file.keys()}
+            elif isinstance(file, h5py.Dataset):
+                return file[start: start + self.obs_horizon]
+            else:
+                raise NotImplementedError(f"H5 file type {type(file)} not supported")
+
+        USED_OBS = ["agent", "extra", "sensor_data"]
+        obs_seq = {key: get_slice_data(self._h5_file[f"{traj_key}/obs/{key}"]) for key in USED_OBS}
+        obs_seq = reorder_keys(obs_seq, self.obs_space)
+        obs_seq = self.obs_process_fn(obs_seq)
+
+        poses_world_peg = self._h5_file[f'{traj_key}/obs/extra/peg_pose'][start: end + 1]
+        poses_cam0_world = self._h5_file[f'{traj_key}/obs/extra/cam0_world_pose'][start: end + 1]
+        act_seq = self._h5_file[f"{traj_key}/actions"][start: end]
+        if end > L:
+            poses_world_peg = np.concatenate([poses_world_peg, poses_world_peg[-1:].repeat(end - L, 0)], axis=0)
+            poses_cam0_world = np.concatenate([poses_cam0_world, poses_cam0_world[-1:].repeat(end - L, 0)], axis=0)
+            act_seq = np.concatenate([act_seq, np.zeros_like(act_seq[-1:]).repeat(end - L, 0)], axis=0)
+
+        return {
+            "observations": obs_seq,
+            "actions": {
+                "poses_world_peg": poses_world_peg,
+                "poses_cam0_world": poses_cam0_world,
+                "raw": act_seq,
+            },
+        }
+
+    def __len__(self):
+        return len(self.slices)
+
+    def __del__(self):
+        if self._h5_file is not None:
+            self._h5_file.close()  # 进程退出时，安全关闭自己的句柄
+            self._h5_file = None
---
+++ examples/baselines/diffusion_policy/train_odpc.py
@@ -0,0 +1,237 @@
+ALGO_NAME = "BC_Diffusion_rgbd_UNet"
+
+import os
+import random
+import time
+from dataclasses import dataclass, field
+from functools import partial
+from typing import List, Optional
+
+import gymnasium as gym
+import numpy as np
+import torch
+import tyro
+
+from mani_skill.utils.wrappers.flatten import FlattenRGBDObservationWrapper
+from mani_skill.utils import common
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data.sampler import BatchSampler, RandomSampler
+from torch.utils.tensorboard import SummaryWriter
+
+from diffusion_policy.make_env import make_eval_envs
+from diffusion_policy.utils import (IterationBasedBatchSampler,
+                                    build_state_obs_extractor, convert_obs,
+                                    worker_init_fn)
+from diffusion_policy.data_converison import DataConversion
+from diffusion_policy.odpc_dataset import ODPCDataset
+
+
+@dataclass
+class Args:
+    exp_name: Optional[str] = None
+    """the name of this experiment"""
+    seed: int = 1
+    """seed of the experiment"""
+    torch_deterministic: bool = True
+    """if toggled, `torch.backends.cudnn.deterministic=False`"""
+    cuda: bool = True
+    """if toggled, cuda will be enabled by default"""
+    track: bool = False
+    """if toggled, this experiment will be tracked with Weights and Biases"""
+    wandb_project_name: str = "ManiSkill"
+    """the wandb's project name"""
+    wandb_entity: Optional[str] = None
+    """the entity (team) of wandb's project"""
+    capture_video: bool = True
+    """whether to capture videos of the agent performances (check out `videos` folder)"""
+
+    env_id: str = "PegInsertionSide-v2"
+    """the id of the environment"""
+    demo_path: str = "demos/PegInsertionSide-v1/trajectory.state.pd_ee_delta_pose.physx_cpu.h5"
+    """the path of demo dataset, it is expected to be a ManiSkill dataset h5py format file"""
+    val_id_demo_path: str = "demos/PegInsertionSide-v1/trajectory.state.pd_ee_delta_pose.physx_cpu.h5"
+    """the path of in-domain validation dataset"""
+    val_ood_demo_path: str = "demos/PegInsertionSide-v1/trajectory.state.pd_ee_delta_pose.physx_cpu.h5"
+    """the path of out-of-domain validation dataset"""
+
+    num_demos: Optional[int] = None
+    """number of trajectories to load from the demo dataset"""
+    total_iters: int = 400_000
+    """total timesteps of the experiment"""
+    batch_size: int = 256
+    """the batch size of sample from the replay memory"""
+
+    # Diffusion Policy specific arguments
+    lr: float = 1e-4
+    """the learning rate of the diffusion policy"""
+    obs_horizon: int = 1  # Seems not very important in ManiSkill, 1, 2, 4 work well
+    act_horizon: int = 8  # Seems not very important in ManiSkill, 4, 8, 15 work well
+    pred_horizon: int = (
+        16
+        # 16->8 leads to worse performance, maybe it is like generate a half image; 16->32, improvement is very marginal
+    )
+    diffusion_step_embed_dim: int = 64  # not very important
+    unet_dims: List[int] = field(
+        default_factory=lambda: [64, 128, 256]
+    )  # default setting is about ~4.5M params
+    n_groups: int = (
+        8  # jigu says it is better to let each group have at least 8 channels; it seems 4 and 8 are simila
+    )
+
+    # Environment/experiment specific arguments
+    obs_mode: str = "rgb+depth"
+    """The observation mode to use for the environment, which dictates what visual inputs to pass to the model. Can be "rgb", "depth", or "rgb+depth"."""
+    max_episode_steps: Optional[int] = 300
+    """Change the environments' max_episode_steps to this value. Sometimes necessary if the demonstrations being imitated are too short. Typically the default
+    max episode steps of environments in ManiSkill are tuned lower so reinforcement learning agents can learn faster."""
+    log_freq: int = 1000
+    """the frequency of logging the training metrics"""
+    eval_freq: int = 40_000
+    """the frequency of evaluating the agent on the evaluation environments"""
+    save_freq: Optional[int] = None
+    """the frequency of saving the model checkpoints. By default this is None and will only save checkpoints based on the best evaluation metrics."""
+    num_eval_episodes: int = 100
+    """the number of episodes to evaluate the agent on"""
+    num_eval_envs: int = 10
+    """the number of parallel environments to evaluate the agent on"""
+    sim_backend: str = "physx_cpu"
+    """the simulation backend to use for evaluation environments. can be "cpu" or "gpu"""
+    num_dataload_workers: int = 0
+    """the number of workers to use for loading the training data in the torch dataloader"""
+    control_mode: str = "pd_joint_delta_pos"
+    """the control mode to use for the evaluation environments. Must match the control mode of the demonstration dataset."""
+
+    # Observation process arguments
+    depth_clamp: int = 3000
+    """Clamp depth value to [0, threshold], depth value beyond threshold will be set to 0."""
+    used_cameras: str = "all"
+    """Camera used as DP input. Can be camera ids, such as "0", "0,1,2,"."""
+    use_state: bool = True
+    """Whether to use state as DP input."""
+
+    # additional tags/configs for logging purposes to wandb and shared comparisons with other algorithms
+    demo_type: Optional[str] = None
+
+
+if __name__ == "__main__":
+    args = tyro.cli(Args)
+
+    if args.exp_name is None:
+        args.exp_name = os.path.basename(__file__)[: -len(".py")]
+        run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    else:
+        run_name = args.exp_name
+
+    if args.demo_path.endswith(".h5"):
+        import json
+
+        json_file = args.demo_path[:-2] + "json"
+        with open(json_file, "r") as f:
+            demo_info = json.load(f)
+            if "control_mode" in demo_info["env_info"]["env_kwargs"]:
+                control_mode = demo_info["env_info"]["env_kwargs"]["control_mode"]
+            elif "control_mode" in demo_info["episodes"][0]:
+                control_mode = demo_info["episodes"][0]["control_mode"]
+            else:
+                raise Exception("Control mode not found in json")
+            assert (
+                    control_mode == args.control_mode
+            ), f"Control mode mismatched. Dataset has control mode {control_mode}, but args has control mode {args.control_mode}"
+    assert args.obs_horizon + args.act_horizon - 1 <= args.pred_horizon
+    assert args.obs_horizon >= 1 and args.act_horizon >= 1 and args.pred_horizon >= 1
+
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+
+    # create evaluation environment
+    env_kwargs = dict(
+        control_mode=args.control_mode,
+        reward_mode="sparse",
+        obs_mode=args.obs_mode,
+        render_mode="rgb_array",
+        human_render_camera_configs=dict(shader_pack="default"),
+        sensor_configs=dict(shader_pack="default"),
+    )
+    assert args.max_episode_steps != None, "max_episode_steps must be specified as imitation learning algorithms task solve speed is dependent on the data you train on"
+    env_kwargs["max_episode_steps"] = args.max_episode_steps
+    other_kwargs = dict(obs_horizon=args.obs_horizon)
+    envs = make_eval_envs(
+        args.env_id,
+        args.num_eval_envs,
+        args.sim_backend,
+        env_kwargs,
+        other_kwargs,
+        video_dir=f"runs/{run_name}/videos" if args.capture_video else None,
+        wrappers=[FlattenRGBDObservationWrapper],
+    )
+
+    if args.track:
+        import wandb
+
+        config = vars(args)
+        config["eval_env_cfg"] = dict(**env_kwargs, num_envs=args.num_eval_envs, env_id=args.env_id,
+                                      env_horizon=args.max_episode_steps)
+        wandb.init(
+            project=args.wandb_project_name,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=config,
+            name=run_name,
+            save_code=True,
+            group="DiffusionPolicy",
+            tags=["diffusion_policy"],
+        )
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s"
+        % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
+
+    obs_process_fn = partial(
+        convert_obs,
+        concat_fn=partial(np.concatenate, axis=-1),
+        transpose_fn=partial(
+            np.transpose, axes=(0, 3, 1, 2)
+        ),  # (B, H, W, C) -> (B, C, H, W)
+        state_obs_extractor=build_state_obs_extractor(args.env_id),
+        depth="rgbd" in args.obs_mode or "depth" in args.obs_mode,
+    )
+
+    # create temporary env to get original observation space as AsyncVectorEnv (CPU parallelization) doesn't permit that
+    tmp_env = gym.make(args.env_id, **env_kwargs)
+    orignal_obs_space = tmp_env.observation_space
+    # determine whether the env will return rgb and/or depth data
+    include_rgb = tmp_env.unwrapped.obs_mode_struct.visual.rgb
+    include_depth = tmp_env.unwrapped.obs_mode_struct.visual.depth
+    tmp_env.close()
+
+    dataset = ODPCDataset(
+        args=args,
+        data_path=args.demo_path,
+        obs_process_fn=obs_process_fn,
+        obs_space=orignal_obs_space,
+        include_rgb=include_rgb,
+        include_depth=include_depth,
+        num_traj=args.num_demos
+    )
+    sampler = RandomSampler(dataset, replacement=False)
+    batch_sampler = BatchSampler(sampler, batch_size=args.batch_size, drop_last=True)
+    batch_sampler = IterationBasedBatchSampler(batch_sampler, args.total_iters)
+    train_dataloader = DataLoader(
+        dataset,
+        batch_sampler=batch_sampler,
+        num_workers=args.num_dataload_workers,
+        worker_init_fn=lambda worker_id: worker_init_fn(worker_id, base_seed=args.seed),
+        persistent_workers=(args.num_dataload_workers > 0),
+    )
+
+    data_conversion = DataConversion()
+
+    for iteration, data_batch in enumerate(train_dataloader):
+        data_batch = common.to_tensor(data_batch, device)
+        print(common.get_tensor_shape(data_batch))
