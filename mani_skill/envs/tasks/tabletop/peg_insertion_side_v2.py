32b700364fac39f6aa44143369362b6af4e33de3
Xu Cuijie <975114697@qq.com>
2025-05-16 17:38:19 +0800

    相机随机位置初始化

--- mani_skill/envs/tasks/tabletop/peg_insertion_side_v2.py
+++ mani_skill/envs/tasks/tabletop/peg_insertion_side_v2.py
@@ -38,14 +38,14 @@ class PandaForPegInsertion(Panda):
 
 @register_env("PegInsertionSide-v2", max_episode_steps=100)
 class PegInsertionSideV2Env(PegInsertionSideEnv):
-    SUPPORTED_ROBOTS = ["panda_peg_insertion", "xarm6_robotiq"]
+    SUPPORTED_ROBOTS = ["panda", "panda_peg_insertion", "xarm6_robotiq"]
     agent: Union[PandaForPegInsertion]
     _clearance = 0.005
 
     def __init__(
             self,
             *args,
-            robot_uids="panda_peg_insertion",
+            robot_uids="panda",
             **kwargs,
     ):
         super().__init__(*args, robot_uids=robot_uids, **kwargs)
@@ -56,8 +56,18 @@ class PegInsertionSideV2Env(PegInsertionSideEnv):
 
     @property
     def _default_sensor_configs(self):
-        pose = sapien_utils.look_at([0.4, -0.4, 0.4], [0.05, -0.1, 0.2])
+        eye = np.random.uniform(low=[0.4, -0.4, 0.5], high=[0.7, -0.2, 1.0])
+        target = np.random.uniform(low=[-0.1, 0, 0], high=[0.1, 0.3, 0.6 * eye[2]])
+        pose = sapien_utils.look_at(eye, target)
         return [CameraConfig("base_camera", pose, 256, 256, np.pi / 2, 0.01, 100)]
+
+    @property
+    def _default_human_render_camera_configs(self):
+        eye = np.random.uniform(low=[0.4, -0.4, 0.5], high=[0.7, -0.2, 1.0])
+        target = np.random.uniform(low=[-0.1, 0, 0], high=[0.1, 0.3, 0.6 * eye[2]])
+        pose = sapien_utils.look_at(eye, target)
+        return CameraConfig("render_camera", pose, 512, 512, 1, 0.01, 100)
+
 
     def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
         with torch.device(self.device):
