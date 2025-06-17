93e4cac57e038009e477aa816b11c94f1764bf94
Xu Cuijie <975114697@qq.com>
2025-06-17 16:13:51 +0800

    vis peg pose新增headless模式

--- odpc/scripts/vis/peg_pose.py
+++ odpc/scripts/vis/peg_pose.py
@@ -14,6 +14,7 @@ from odpc.utils.visualize import visualize_pose
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--traj-path', type=str, default='demos/peg_insertion_demo.state_dict+rgb+depth+segmentation.pd_ee_pose.physx_cpu.h5')
+    parser.add_argument('--headless', action='store_true')
     return parser.parse_args()
 
 
@@ -23,6 +24,12 @@ if __name__ == "__main__":
 
     with h5py.File(args.traj_path, 'r') as file:
         
+        if args.headless:
+            h, w = file[list(file.keys())[0]]['obs/sensor_data/base_camera/rgb'].shape[1:3]
+            writer = cv2.VideoWriter(f'peg_pose.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 50, (w, h))
+       
+        index = 0
+
         for traj_key in file.keys():
             cam0_peg_poses = file[f'{traj_key}/obs/extra/cam0_peg_pose'][:]
             images_raw = file[f'{traj_key}/obs/sensor_data/base_camera/rgb'][:]
@@ -33,13 +40,25 @@ if __name__ == "__main__":
 
             for pose, rgb in zip(cam0_peg_poses, images_raw):
                 vis = visualize_pose(rgb, pose, intrinsic)
-                cv2.imshow('vis', vis[:, :, ::-1])
-                key = cv2.waitKey(10)
-                if key == 27 or key == ord('q'):
+                index += 1
+                print(index, end='\r')
+                if args.headless:
+                    writer.write(vis[:, :, ::-1])
+                    if index > 5000:
+                        break
+                else:
+                    cv2.imshow('vis', vis[:, :, ::-1])
+                    key = cv2.waitKey(10)
+                    if key == 27 or key == ord('q'):
+                        break
+            if args.headless:
+                if index > 5000:
                     break
-            
-            if key == 27 or key == ord('q'):
+            elif key == 27 or key == ord('q'):
                 break
 
-        cv2.destroyAllWindows()
+        if args.headless:
+            writer.release()
+        else:
+            cv2.destroyAllWindows()
             
