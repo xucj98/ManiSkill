32b9e571ed5b0c49b27e40ea3bc9ccf863f262d5
Xu Cuijie <975114697@qq.com>
2025-07-08 14:24:58 +0800

    精度诅咒新增数值处理方法：SequentialTransform，SeriesRemoveOutliersTransform

--- odpc/odpc/evaluation/precision_curse/transforms.py
+++ odpc/odpc/evaluation/precision_curse/transforms.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
-from typing import Any, Optional, Dict, Union
+from typing import Any, Optional, Dict, Union, List
 
+from omegaconf import DictConfig
+
+from odpc.utils.utils import instantiate_from_config
 
 class BaseTransform:
     def __init__(self, **kwargs): 
@@ -51,6 +54,28 @@ class SeriesAggregationTransform(BaseTransform):
         return None
 
 
+class SeriesRemoveOutliersTransform(BaseTransform):
+    def __init__(self, method: str, args: Optional[Dict] = None):
+        super().__init__()
+        self.method = method
+        self.args = args or {}
+        
+    def __call__(self, series: pd.Series) -> pd.Series:
+        if self.method == "max_n":
+            n = self.args.get("n", 1)
+            # 从序列中删除最大的n个元素
+            if len(series) <= n:
+                return series
+            max_indices = series.nlargest(n).index
+            return series.drop(max_indices)
+        if self.method == "min_n":
+            n = self.args.get("n", 1)
+            if len(series) <= n:
+                return series
+            min_indices = series.nsmallest(n).index
+            return series.drop(min_indices)
+
+
 class MultiplyTransform(BaseTransform):
     def __init__(self, factor: float):
         super().__init__()
@@ -98,3 +123,20 @@ class InverseShiftedTransform(BaseTransform):
     def inverse(self, value: Union[float, int, pd.Series]) -> float:
         return self.shift + 1 / value
 
+
+class SequentialTransform(BaseTransform):
+    def __init__(self, transforms: List[DictConfig]):
+        super().__init__()
+        self.transforms: List[BaseTransform] = []
+        for transform in transforms:
+            self.transforms.append(instantiate_from_config(transform))
+
+    def __call__(self, value: Any) -> Any:
+        for transform in self.transforms:
+            value = transform(value)
+        return value
+
+    def inverse(self, value: Any) -> Any:
+        for transform in reversed(self.transforms):
+            value = transform.inverse(value)
+        return value
